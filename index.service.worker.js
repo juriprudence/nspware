fbee20796c88c902712cb13b5a08b949948a24d5
juriprudence <ramedde40071@gmail.com>
2026-02-14 23:06:46 +0100

    ee

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771106018|1205338846';
+const CACHE_VERSION = '1771106785|1972033839';
 /** @type {string} */
 const CACHE_PREFIX = 'Shooter-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
